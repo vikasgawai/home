65f50185275cb283329216b0af4acd32dc6f57c3
Vikas PD Gawai <57414637+vikasgawai@users.noreply.github.com>
2023-10-27 15:11:07 -0500

    hi

--- cover_letters/Harvard_post_doc/Faculty Members – Harvard Center for Population and Development Studies_files/hsph-table-sortable.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($) {
-    // Wrapping all tables with a wrapper to make large table scrollable on mobile.
-    $( '#main table' ).wrap( '<div class="responsive-table"></div>' );
-    // And load our datatables into the page.
-    $( '#main table.sortable' ).tablesorter();
-});
